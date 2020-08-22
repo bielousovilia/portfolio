70442e430fe3e082c0d74e3074629aa7a3a24457
Ilia Bielousov <bielousov.ilia@gmail.com>
2020-08-22 14:11:25 +0300

    Скрипт для фильтрации работ

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,13 +3,22 @@
 window.addEventListener('DOMContentLoaded', () => {
   // !BURGER MENU 
   const burger = document.querySelector('.burger'),
-    menu = document.querySelector('.menu');
+    menu = document.querySelector('.menu'),
+    menuItems = menu.querySelectorAll('.menu__link');
 
   burger.addEventListener('click', () => {
     menu.classList.toggle('active');
     document.body.classList.toggle('body-lock');
   });
 
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      if (window.screen.width <= 769) {
+        menu.classList.toggle('active');
+        document.body.classList.toggle('body-lock');
+      }
+    });
+  });
 
   // !SCROLLING
   const links = menu.querySelectorAll('[href^="#"]'),
@@ -19,60 +28,104 @@ window.addEventListener('DOMContentLoaded', () => {
     pos = [],
     speed;
 
-    links.forEach(link => {
-      arr.push(link.hash);
-    });
+  links.forEach(link => {
+    arr.push(link.hash);
+  });
 
-    const elements = document.querySelectorAll(arr);
+  const elements = document.querySelectorAll(arr);
 
-    elements.forEach(elem => {
-      pos.push(elem.getBoundingClientRect().top);
-    });
+  elements.forEach(elem => {
+    pos.push(elem.getBoundingClientRect().top);
+  });
 
-    console.log(pos);
+  console.log(pos);
 
-    function widthDevice() {
-      if (window.screen.width >= 768) {
-        speed = 4;
+  function widthDevice() {
+    if (window.screen.width >= 769) {
+      speed = 4;
+    } else {
+      speed = 7;
+    }
+  }
+
+  widthDevice();
+
+  links.forEach((link, i) => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      let scrollWindow = document.documentElement.scrollTop;
+      let y = pos[i] - header.clientHeight;
+
+      if (scrollWindow > y) {
+        step(y, scrollWindow, '-', speed);
       } else {
-        speed = 7;
+        step(y, scrollWindow, '+', speed);
       }
+
+      function step(position, positionNow, sign, speed) {
+        let timer = setInterval(() => {
+          if (sign === '-') {
+            if (Math.round(positionNow) >= Math.round(position)) {
+              positionNow -= speed;
+              document.documentElement.scrollTop = positionNow;
+            } else {
+              timer = clearInterval();
+            }
+          } else {
+            if (Math.round(positionNow) <= Math.round(position)) {
+              positionNow += speed;
+              document.documentElement.scrollTop = positionNow;
+            } else {
+              timer = clearInterval();
+            }
+          }
+        }, 4);
+      }
+    });
+  });
+
+  // !FILTER
+  const projects = document.querySelectorAll('.portfolio__parent'),
+    projectsLinks = document.querySelectorAll('.portfolio__item'),
+    parent = document.querySelector('.parent');
+
+    async function func(item, display = 'none') {
+        item.style.display = display;
     }
 
-    widthDevice();
+  function showItems(attr = 'none') {
+    projects.forEach(project => {
+      project.classList.remove('active-filter');
+      project.classList.add('noactive-filter');
+      func(project);
 
-    links.forEach((link, i) => {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        
-        let scrollWindow = document.documentElement.scrollTop;
-        let y = pos[i] - header.clientHeight;
+      if (project.hasAttribute(attr)) {
+        project.classList.remove('noactive-filter');
+        project.classList.add('active-filter');
+        func(project, 'block');
+        parent.classList.add('items');
+      }
 
-        if (scrollWindow > y) {
-          step(y, scrollWindow, '-', speed);
-        } else {
-          step(y, scrollWindow, '+', speed);
-        }
-
-        function step(position, positionNow, sign, speed) {
-          let timer = setInterval(() => {
-            if (sign === '-') {
-              if (Math.round(positionNow) >= Math.round(position)) {
-                positionNow -= speed;
-                document.documentElement.scrollTop = positionNow;
-              } else {
-                timer = clearInterval();
-              }
-            } else {
-              if (Math.round(positionNow) <= Math.round(position)) {
-                positionNow += speed;
-                document.documentElement.scrollTop = positionNow;
-              } else {
-                timer = clearInterval();
-              }
-            }
-          }, 4);
-        }
-      });
+      if (attr === 'none') {
+        project.classList.remove('noactive-filter');
+        project.classList.add('active-filter');
+        func(project, 'block');
+        parent.classList.remove('items');
+      }
     });
+  }
+
+  projectsLinks.forEach(item => {
+    item.addEventListener('click', function () {
+      if (item.hasAttribute('data-frontend')) {
+        showItems('data-frontend');
+      } else if (item.hasAttribute('data-website')) {
+        showItems('data-website');
+      } else {
+        showItems();
+      }
+    });
+  });
+
 });
